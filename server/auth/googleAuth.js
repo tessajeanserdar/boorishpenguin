ba3e29c486fa0832cade2d989f489fb20c7ec13d
giselakayy <giselakottmeier@gmail.com>
2016-01-07 14:20:09 -0800

    Google OAuth implemented

---
+++ server/auth/googleAuth.js
@@ -0,0 +1,42 @@
+var passport = require('passport')
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var apikeys = require('../config/apikeys.js');
+
+exports.ensureAuth = function (req, res, next){
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/auth/google');
+}
+exports.verify = function (profileObj, callback){
+  var user = {};
+  user.displayName = profileObj.profile.displayName;
+  user.lastName = profileObj.profile.name.familyName;
+  user.firstName = profileObj.profile.name.givenName;
+  user.fullName = user.firstName+" "+user.lastName;
+  user.email = profileObj.profile.emails[0].value;
+  user.gender = profileObj.profile.gender;
+  user.nickName = profileObj.profile._json.nickname;
+  user.image = profileObj.profile._json.image.url;
+  return callback(null, user);
+}
+exports.serializeUser = function (){
+  passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+
+  passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+  });
+}
+exports.oauth = function (){
+  passport.use(new GoogleStrategy({
+    clientID: apikeys.googleOauth.clientID,
+    clientSecret: apikeys.googleOauth.clientSecret,
+    callbackURL: "http://127.0.0.1:8001/auth/google/callback",
+  },
+    function(accessToken, refreshToken, profile, done) {
+      exports.verify({profile: profile}, function (err, profile){
+        return done(err, profile)
+      })
+    }
+  ));
+}
--- server/server.js
+++ server/server.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var googleAuth = require('./auth/googleAuth.js')
+var passport = require('passport')
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 var app = express();
 var port = process.env.PORT || 8001;
@@ -7,7 +12,15 @@ var port = process.env.PORT || 8001;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/../client'));
-require('./config/routes.js')(app, express);
+app.use(cookieParser());
+app.use(session({ secret: 'hi' , resave: true, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session());
+require('./config/routes.js')(app, express, googleAuth.ensureAuth);
 
 app.listen(port);
 module.exports = app;
+
+googleAuth.serializeUser();
+googleAuth.oauth();
+
