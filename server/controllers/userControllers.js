73d116f70a9b7707a36e23920d22157c03facea6
Erik Brown <erikdbrown@gmail.com>
2016-01-06 22:18:16 -0800

    created placeholder methods so that I can use the server

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,1 +1,10 @@
 // user controllers go in here
+module.exports = {
+  allUsers: function() {
+    console.log('request for all users received');
+  },
+
+  newUser: function() {
+    console.log('new user received');
+  }
+}
