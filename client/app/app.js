93121bea86d26b4a3770f2b7eaa2a733fc0e64d4
Erik Brown <erikdbrown@gmail.com>
2016-01-06 17:24:10 -0800

    added questions.js questions.html client/index.html preparing to merge

---
+++ client/app/app.js
@@ -0,0 +1,3 @@
+angular.module('boorishpenguin', [
+  'boorish.question'
+  ])
---
+++ client/app/questions/questions.js
@@ -0,0 +1,45 @@
+angular.module('boorish.question', [])
+
+.factory('Questions', function($http) {
+  return {
+    // question is a string 
+    addQuestion: function(question) {
+      $http({
+        method: 'POST',
+        url: 'townhall/questions',
+        data: JSON.stringify({
+          text: question.question,
+          name: question.user,
+          isAnswered: false,
+          isGood: false,
+          points: 0
+        })
+      })
+      .then(function() {
+        console.log('question sent');
+      })
+    },
+
+    getAnswers: function(question) {
+      $http({
+        method: 'GET',
+        url: 'api/questions',
+      }).then(function() {
+        return res.data
+      })
+    }
+  }
+
+})
+
+.controller('questionController', function($scope) {
+  $scope.question = {};
+  $scope.answers = [];
+
+  $scope.getUser = function() {
+    // grabs the username from wherever we're storing it.
+    // sets username to 
+      // $scope.question.user
+  }
+
+})
