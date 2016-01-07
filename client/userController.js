f51f1dba93962f286e13af9c0146b12c4235d5a8
justshiv <accounts@justshiv.com>
2016-01-06 19:54:25 -0800

    stubbed out users front-end handling

--- client/userController.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by siobhan on 2016/01/06.
- */
-angular.module('boorishpenguin', [])
-  .controller('UserController', function($scope, $http){
-    $scope.users = [{'name': 'Jane'}, {'name': 'Paul'}];
-  });
