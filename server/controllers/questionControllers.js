fd05c7c22acdfacf27be491a8fc0bbf95d8037e1
Steven Law <slaw@nuclearanvil.com>
2016-01-07 13:32:19 -0800

    Question controllers created. Need auth checks on delete and mod.

--- server/controllers/questionControllers.js
+++ server/controllers/questionControllers.js
@@ -19,18 +19,62 @@ module.exports = {
         }
       });
 
-      questions = {}
+      questions = {};
       questions.results = formattedQs;
       res.json(questions);
     });
   },
 
   newQuestion: function(req, res) {
-    console.log('new question received', req.body);
+    var txt = req.body.text;
+    var uname = req.body.username;
+    var coursename = req.body.course;
+    var tagname = req.body.tag;
+
+    db.User.findOrCreate({
+      where: {
+        username: uname
+      }
+    })
+    .spread(function(user, created) {
+      db.Course.findOrCreate({
+        where: {
+          name: coursename
+        }
+      })
+      .spread(function(course, created) {
+        db.Tag.findOrCreate({
+          where: {
+            name: tagname
+          }
+        })
+        .spread(function(tag, created) {
+          db.Question.create({
+            text: txt,
+            UserId: user.get('id'),
+            CourseId: course.get('id'),
+            TagId: tag.get('id')
+          })
+          .then(function(message) {
+            res.sendStatus(201);
+          });
+        });
+      });
+    });
   },
 
-  deleteQuestion: function() {
-    console.log('request to delete question received');
+  // TODO: add check for admin or same-user
+  deleteQuestion: function(req, res) {
+    db.Question.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(function(id) {
+      if (id) {
+        res.sendStatus(204);
+      };
+    });
   },
 
   readQuestion: function(req, res) {
@@ -84,7 +128,72 @@ module.exports = {
     })
   },
 
-  modQuestion: function() {
-    console.log('post request to modify a question received');
+  // TODO: add check for admin or same-user
+  modQuestion: function(req, res) {
+    var qid = req.params.id;
+    var mod = req.body.mod;
+
+    db.Question.findAll({
+      where: {
+        id: qid
+      }
+    })
+    .then(function(questions) {
+      var curGood = questions[0].isGood;
+      var curLike = questions[0].points;
+      var curClosed = questions[0].isClosed;
+      var curAnswered = questions[0].isAnswered;
+    });
+
+    // anybody can like; doesn't need auth
+    if (mod === 'like') {
+      db.Question.update({
+        points: curLike + 1;
+      }, {
+        where: {
+          id: qid
+        }
+      })
+      .then(function() {
+        res.sendStatus(201);
+      })
+    } else if (mod === 'good') {
+      // admin only
+      db.Question.update({
+        isGood: !curGood;
+      }, {
+        where: {
+          id: qid
+        }
+      })
+      .then(function() {
+        res.sendStatus(201);
+      });
+    } else if (mod === 'closed') {
+      // admin only; maybe refactor this and the previous
+      // to run the admin check first?
+      db.Question.update({
+        isClosed: !curClosed;
+      }, {
+        where: {
+          id: qid
+        }
+      })
+      .then(function() {
+        res.sendStatus(201);
+      });
+    } else if (mod === 'answered') {
+      // same-user only
+      db.Question.update({
+        isAnswered: !curAnswered;
+      }, {
+        where: {
+          id: qid
+        }
+      })
+      .then(function() {
+        res.sendStatus(201);
+      })
+    }
   }
 };
