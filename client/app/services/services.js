628eb73e9eefef2ba37ce7a63552dd49650c2047
Erik Brown <erikdbrown@gmail.com>
2016-01-07 19:04:17 -0800

    added Auth services template

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -186,3 +186,46 @@ angular.module('boorish.services', [])
 
   });
 
+.factory('Auth', function ($http, $location, $window) {
+  // Don't touch this Auth service!!!
+  // it is responsible for authenticating our user
+  // by exchanging the user's username and password
+  // for a JWT from the server
+  // that JWT is then stored in localStorage as 'com.shortly'
+  // after you signin/signup open devtools, click resources,
+  // then localStorage and you'll see your token from the server
+  return { 
+    signin: function (user) {
+    return $http({
+      method: 'POST',
+      url: '/api/users/signin',
+      data: user
+    })
+    .then(function (resp) {
+      return resp.data.token;
+    });
+  },
+
+  signup: function (user) {
+    return $http({
+      method: 'POST',
+      url: '/api/users/signup',
+      data: user
+    })
+    .then(function (resp) {
+      return resp.data.token;
+    });
+  },
+
+  isAuth: function () {
+    return !!$window.localStorage.getItem('com.shortly');
+  },
+
+  signout: function () {
+    $window.localStorage.removeItem('com.shortly');
+    $location.path('/signin');
+  }
+}
+
+});
+
