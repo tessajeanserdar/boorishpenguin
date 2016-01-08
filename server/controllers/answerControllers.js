02f78123925293ce0b5502aff997176922c5bfba
Steven Law <slaw@nuclearanvil.com>
2016-01-07 16:05:39 -0800

    Created controller function for adding answer.

--- server/controllers/answerControllers.js
+++ server/controllers/answerControllers.js
@@ -1,14 +1,40 @@
-// answer controllers go in here
+var db = require('../db/index.js');
+
 module.exports = {
   newAnswer: function(req, res) {
-    console.log('new Answer received', req.body);
+    var txt = req.body.text;
+    var uname = req.body.person;
+    var qid = req.body.id_Question;
+    
+    db.User.findOne({
+      where: {
+        username: uname,
+      }
+    })
+    .then(function(user) {
+      return db.Answer.create({
+        text: txt,
+        UserId: user.get('id'),
+        QuestionId: qid;
+      })
+    })
+    .then(function() {
+      return user.update({
+        points: user.get('points') + 1
+      })
+    })
+    .then(function() {
+      res.sendStatus(201);
+    });
   },
 
-  modAnswer: function() {
-    console.log('request to modify answer received');
+  modAnswer: function(req, res) {
+    var aid = req.params.id;
+    var mod = req.body.mod;
+
   },
 
-  deleteAnswer: function() {
+  deleteAnswer: function(req, res) {
     console.log('request to delete answer received');
   }
 }
