a94b9a8f2195ba71e1ff926123dff1934d0840d8
Steven Law <slaw@nuclearanvil.com>
2016-01-07 19:57:13 -0800

    allUsers controller.

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,10 +1,29 @@
-// user controllers go in here
+var db = require('../db/index.js');
+
 module.exports = {
-  allUsers: function() {
-    console.log('request for all users received');
+  allUsers: function(req, res) {
+    var course = req.body.coursename;
+
+    db.User.findAll()
+    .then(function(users)) {
+      var formattedUsers = users.map(function(user) {
+        return {
+          id: user.id,
+          schoolId: user.schoolId,
+          name: user.name,
+          email: user.email,
+          points: user.points,
+          image: user.image
+        }
+      });
+
+      users = {};
+      users.results = formattedUsers;
+      res.json(users);
+    }
   },
 
-  newUser: function() {
+  newUser: function(req, res) {
     console.log('new user received');
   }
 }
