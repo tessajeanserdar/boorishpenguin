361da91fbff3e6ecf4b0c2f3e3d2551b88e4e54e
Erik Brown <erikdbrown@gmail.com>
2016-01-09 16:45:34 -0800

    passes in questionID to mod answer

--- client/app/answers/answers.js
+++ client/app/answers/answers.js
@@ -13,12 +13,14 @@ angular.module('boorish.answers', [])
   };
 
   $scope.addAnswer = function() {
-    Answers.addAnswer($scope.newAnswer).then(function() {
+    var id_question = $scope.data.question.id;
+    Answers.addAnswer($scope.newAnswer, id_question).then(function() {
       $scope.getQuestion();
     })
   };
 
   $scope.updateAnswer = function(index, mod) {
+    // mod is 'good' or 'like'
     var answerID = $scope.data.answers[index].id;
     Answers.updateAnswer(answerID, mod).then(function() {
       $scope.getQuestion();
