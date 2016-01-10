280ba35e974231e07f3f7c0a22c26c8cbc1f8a71
giselakayy <giselakottmeier@gmail.com>
2016-01-09 19:29:29 -0800

    Should add to users to database

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var apikeys = require('./config/apikeys.js');
-
+var controllers = require('./controllers/userControllers.js');
 var app = express();
 var port = process.env.PORT || 8001;
 
@@ -36,7 +36,15 @@ passport.use(new GoogleStrategy({
   callbackURL: "http://127.0.0.1:8001/auth/google/callback",
 },
   function(accessToken, refreshToken, profile, done) {
-    // if (controllers.isUserInDb(profile.emails[0].value)) {
+    if (controllers.isUserInDb(profile.emails[0].value)) {
+      googleAuth.login({profile: profile}, function (err, profile){
+        return done(err, profile);
+      });
+    } else {
+      googleAuth.signup({profile: profile}, function (err, profile){
+        return done(err, profile);
+    });
+    
     //   exports.login({profile: profile}, function (err, profile){
     //     return done(err, profile);
     //   });
@@ -44,13 +52,11 @@ passport.use(new GoogleStrategy({
       // exports.signup({profile: profile}, function (err, profile){
       //   return done(err, profile);
       // });
-    // }
+    
     // if user in database
     // else
-    googleAuth.signup({profile: profile}, function (err, profile){
-      return done(err, profile);
-    });
+
     
-  }
+  }}
 ));
 
