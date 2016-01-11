011c01112d18a013ea55ca46ef1be05c4c505f7a
Steven Law <slaw@nuclearanvil.com>
2016-01-10 22:16:02 -0800

    Pulling in tag and course data.

--- client/app/ask/ask.js
+++ client/app/ask/ask.js
@@ -1,11 +1,32 @@
 angular.module('boorish.ask', [])
 
-.controller('askController', function($scope, $window, $location, Questions) {
+.controller('askController', function($scope, $window, $location, Tags, Courses, Questions) {
   $scope.question = {};
+
+  Tags.getTags()
+  .then(function(data) {
+    $scope.tagOptions = {
+      availableOptions: data.results,
+      selectedOption: data.results[data.results.length - 1]
+    }
+    console.log($scope.tagOptions);
+    return;
+  })
+  .then(function() {
+    return Courses.getCourses();
+  })
+  .then(function(data) {
+    $scope.courseOptions = {
+      availableOptions: data.results,
+      selectedOption: data.results[data.results.length - 1]
+    };
+  });
 
   $scope.addQuestion = function() {
     $scope.question.userId = $window.localStorage.getItem('com.boorish');
     // TODO: need to pull username into question object
+    $scope.question.course = $scope.courseOptions.selectedOption.name;
+    $scope.question.tag = $scope.tagOptions.selectedOption.name;
     Questions.addQuestion($scope.question);
     $location.path('/questions');
 
