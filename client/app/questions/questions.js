c1edf0427bf762b9693b476aeb0db320455b5fa1
Erik Brown <erikdbrown@gmail.com>
2016-01-11 15:23:17 -0800

    removed unecessary mapping of data now that we store a title

--- client/app/questions/questions.js
+++ client/app/questions/questions.js
@@ -7,10 +7,7 @@ angular.module('boorish.questions', [])
   $scope.init = function() {
 
     Questions.getAllQuestions().then(function(data) {
-      $scope.questions = data.results.map(function(question) {
-        question.title = question.text.slice(0,60);
-        return question;
-      });
+      $scope.questions = data.results;
     });
   };
 
