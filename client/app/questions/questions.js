3ee1ede0f1a364e60a6af9dab106a4e52b2c72c6
giselakayy <giselakottmeier@gmail.com>
2016-01-11 15:34:54 -0800

    No longer defaults title to first 60 chars

--- client/app/questions/questions.js
+++ client/app/questions/questions.js
@@ -16,7 +16,7 @@ angular.module('boorish.questions', [])
       } else {
         Questions.getAllQuestions().then(function(data) {
           $scope.questions = data.results.map(function(question) {
-            question.title = question.text.slice(0,60);
+            // question.title = question.text.slice(0,60);
             return question;
           });
         });
