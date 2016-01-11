5d6d9aee22ed915156618952c06e3e62417bb93e
Erik Brown <erikdbrown@gmail.com>
2016-01-11 15:21:45 -0800

    modified authentication to /questions. GET request to /auth/google returns a user even though no signed in

--- client/app/questions/questions.js
+++ client/app/questions/questions.js
@@ -2,27 +2,21 @@ angular.module('boorish.questions', [])
 
 .controller('questionsController', function($scope, $location, Questions, Auth) {
   $scope.questions = [];
+  Auth.setUser();
 
   $scope.init = function() {
 
-    if (!Auth.isAuth()) {
-      $location.path('/signin');
-    } else {
-      Auth.setUser();
-      var user = Auth.isAuth();
-
-      if (!user) {
-        $location.path('/signin');
-      } else {
-        Questions.getAllQuestions().then(function(data) {
-          $scope.questions = data.results.map(function(question) {
-            // question.title = question.text.slice(0,60);
-            return question;
-          });
-        });
-      }
-    }
+    Questions.getAllQuestions().then(function(data) {
+      $scope.questions = data.results.map(function(question) {
+        question.title = question.text.slice(0,60);
+        return question;
+      });
+    });
   };
 
-  $scope.init();
+  if (!Auth.isAuth()) {
+    $location.path('/signin')
+  } else {
+    $scope.init();
+  }
 });
