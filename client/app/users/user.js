5ed3ac93d807034c85d0b1001502a3ee08560456
Liza Lopez <liza.b.lopez@gmail.com>
2016-01-13 15:09:43 -0800

    user profile has activity links

--- client/app/users/user.js
+++ client/app/users/user.js
@@ -4,7 +4,7 @@ angular.module('boorish.user', [])
 
     //$scope.data = {}
   $scope.user = {
-    'username': 'theCoolest',
+    'username': 'LizaTheCoolest',
     'name': 'Liza Lopez',
     'name_last': 'Lopez',
     'name_first': 'Liza',
@@ -14,6 +14,8 @@ angular.module('boorish.user', [])
     'picture': 'http://i.imgur.com/8K7s19Q.png'
     };
 
+  //make a call to DB:
+  //SELECT name from courses..inner join courses_students on student.id = coursesStudents.student.id
   $scope.classes = [
     'Advanced Woodshop',
     'Conversational Spanish',
@@ -21,9 +23,11 @@ angular.module('boorish.user', [])
     'Remedial PE',
     'Speech and Debate'
   ];
-    $scope.getUserWithId = function(){
-      Users.getUserWithId().then(function(user){
-        $scope.user = user;
-      });
-    };
+  $scope.getUserWithId = function(){
+    Users.getUserWithId().then(function(user){
+      $scope.user = user;
+    });
+  };
+
+  // $scope.
 });
