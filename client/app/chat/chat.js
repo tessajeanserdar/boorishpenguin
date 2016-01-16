4da27e8f041531aa0f7c12dda920d16293ccae3d
Liza Lopez <liza.b.lopez@gmail.com>
2016-01-16 10:20:21 -0800

    users can post resources and they load on resources page

--- client/app/chat/chat.js
+++ client/app/chat/chat.js
@@ -4,21 +4,20 @@ angular.module('boorish.chat', ['firebase'])
   var ref = new Firebase('https://soymilk.firebaseIO.com/');
   $scope.messages = $firebaseArray(ref);
 
-    $scope.addMessage = function(){
-        var name = $('#nameInput').val();
-        var text = $('#messageInput').val();
-        ref.push({name: name, text: text});
-        $('#messageInput').val('');
-      }
-    });
-    ref.on('child_added', function(snapshot) {
-      var message = snapshot.val();
-      displayChatMessage(message.name, message.text);
-    });
-    function displayChatMessage(name, text) {
-      $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
-      $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
-    };
+  $scope.addMessage = function(){
+      var name = $('#nameInput').val();
+      var text = $('#messageInput').val();
+      ref.push({name: name, text: text});
+      $('#messageInput').val('');
+  };
+  ref.on('child_added', function(snapshot) {
+    var message = snapshot.val();
+    displayChatMessage(message.name, message.text);
+  });
+  function displayChatMessage(name, text) {
+    $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+    $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+  };
 
 
-})
+});
