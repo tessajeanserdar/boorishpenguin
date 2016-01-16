a58e120a8c9ddf84917a48bfd5d3b300bec4f690
Justin Mancherje <mancherje.justin@gmail.com>
2016-01-15 20:02:58 -0800

    update class page

--- server/db/index.js
+++ server/db/index.js
@@ -1,11 +1,11 @@
 var Sequelize = require('sequelize');
 
-// var database = process.env.DATABASE;
-// var dbUser = process.env.DBUSER;
-// var dbPass = process.env.DBPASS;
-// var dbHost = process.env.DBHOST;
+var database = process.env.DATABASE;
+var dbUser = process.env.DBUSER;
+var dbPass = process.env.DBPASS;
+var dbHost = process.env.DBHOST;
 
-var database = 'TEST4';
+var database = 'TEST5';
 var dbUser = 'root';
 var dbPass = '';
 var dbHost = '127.0.0.1';
