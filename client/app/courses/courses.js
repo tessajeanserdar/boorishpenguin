c4ffa3fc3e635a0206d0d64b7bb8c8aff92b4b8f
Justin Mancherje <mancherje.justin@gmail.com>
2016-01-16 11:53:23 -0800

    update course page to show resources and questions in separeate tabs

--- client/app/courses/courses.js
+++ client/app/courses/courses.js
@@ -1,8 +1,19 @@
 angular.module('boorish.courses', [])
 .controller('courseController', function($scope, $location,  $routeParams, $http, Questions, Auth, Courses) {
   
+  // default to show questions, show resources by clicking on resources;
+  $scope.listFilter = 'questions';
+
   Courses.getCourseInfo($routeParams.id).then(function (data ){
-      console.log('course data obj', data)
       $scope.courseInfo = data;
+      $scope.courseInfo.resources = data.results.filter(function (post) {
+        return !!post.isAResource;
+      });
+      $scope.courseInfo.questions = data.results.filter(function (post) {
+        return !post.isAResource;
+      })
+      console.log('course data obj', $scope.courseInfo);
   });
+
+
 })
